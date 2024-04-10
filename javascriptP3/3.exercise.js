bb6b976f36e6896005735bb63da778b970fcbcf3
bonath <vannbonath1@gmail.com>
2024-04-10 21:18:05 +0700

    update

--- javascriptP3/3.exercise.js
+++ javascriptP3/3.exercise.js
@@ -1,16 +1,15 @@
-const fs = require("node:fs");
-function readFileFunction(filepath, callback) {
-  fs.readFile(filepath, "utf8", (err, data) => {
+const fs = require("fs");
+
+function readFile(filePath, callback) {
+  fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
-      console.error(err);
-      return;
+      console.log(err);
+    } else {
+      callback(data);
     }
-    callback(data);
   });
 }
-readFileFunction(
-  "c:/Users/konbr/OneDrive/Desktop/Full stack development bootcamp 4 at sabaicode/Coding in bootcamp 4/javascriptP3/3.exercise.txt",
-  (content) => {
-    console.log(content);
-  }
+readFile(
+  "c:/Users/konbr/OneDrive/Desktop/Full stack development bootcamp 4 at sabaicode/Coding in bootcamp 4/input.txt",
+  data
 );
